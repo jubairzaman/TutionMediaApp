b602e7eaa94b7870f11fbb5d47e23d2ec5d3cb55
jubairZamna01 <jubair.zaman01@gmail.com>
2022-05-16 01:31:31 +0600

    TeacherRegistration

---
+++ src/Pages/TeacherRegistration/TRegistration.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TRegistration = () => {
+    return (
+        <div className='backgroundColor1'>
+
+            <div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default TRegistration;
